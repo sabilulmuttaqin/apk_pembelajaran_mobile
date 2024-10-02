a8f8a5380aa9a41d9f9b109b779fc5c08f53d628
sabilulmuttaqin <126049379+sabilulmuttaqin@users.noreply.github.com>
2024-10-02 09:52:33 +0700

    Update app_routes.dart
    Penambahan Route Profile

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,12 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const COURSE = _Paths.COURSE;
+  static const PROFILE = _Paths.PROFILE;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const COURSE = '/course';
+  static const PROFILE = '/profile';
 }
